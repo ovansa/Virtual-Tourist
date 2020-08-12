a37065262af8cd26c6adb706e0413701ecb7b871
ovansa <aminmuhammad18@gmail.com>
2020-08-12 16:26:55 +0100

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Muhammed Ibrahim on 12/08/2020.
+//  Copyright © 2020 Ovansa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
